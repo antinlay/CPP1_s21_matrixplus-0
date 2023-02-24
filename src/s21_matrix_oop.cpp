f78993e6698ece258cb713e5992f937d9000914a
janiecee <janiecee@debian.com>
2023-02-25 01:28:43 +0700

    develop MOVE NOT WORKING

--- src/s21_matrix_oop.cpp
+++ src/s21_matrix_oop.cpp
@@ -1,21 +1,54 @@
 #include "s21_matrix_oop.h"
 
-#include <string.h>
-
+#include <cstring>
 #include <iostream>
 
 using namespace std;
 
 S21Matrix::S21Matrix() {
+  cout << "Constructor 1" << endl;
   rows_ = 2;
   cols_ = 2;
   aloc_matrix(rows_, cols_);
 }
 
 S21Matrix::S21Matrix(int rows, int cols) {
+  cout << "Constructor 2" << endl;
   rows_ = rows;
   cols_ = cols;
   aloc_matrix(rows_, cols_);
+}
+
+S21Matrix::S21Matrix(const S21Matrix& other)
+    : S21Matrix(other.rows_, other.cols_) {
+  cout << "COPY" << endl;
+  for (int i = 0; i < rows_; i++) {
+    for (int j = 0; j < cols_; j++) {
+      matrix_[i][j] = other.matrix_[i][j];
+    }
+  }
+}
+
+S21Matrix::S21Matrix(const S21Matrix&& other)
+    : S21Matrix(other.rows_, other.cols_) {
+  cout << "MOVE" << endl;
+  rows_ = other.rows_;
+  cols_ = other.cols_;
+  for (int i = 0; i < rows_; i++) {
+    for (int j = 0; j < cols_; j++) {
+      matrix_[i][j] = other.matrix_[i][j];
+    }
+  }
+  null_matrix(other);
+  // other.matrix_ = nullptr;
+}
+
+S21Matrix::~S21Matrix() {
+  cout << "Destructor RUN" << endl;
+  for (int i = 0; i < rows_; i++) {
+    delete[] matrix_[i];
+  }
+  delete[] matrix_;
 }
 
 void S21Matrix::aloc_matrix(int rows, int cols) {
@@ -26,24 +59,34 @@ void S21Matrix::aloc_matrix(int rows, int cols) {
   }
 }
 
+void S21Matrix::null_matrix(S21Matrix other) {
+  other.rows_ = 0;
+  other.cols_ = 0;
+  other.matrix_ = nullptr;
+}
+
 double& S21Matrix::operator()(int i, int j) { return matrix_[i][j]; }
 
 int main(void) {
   int rows = 2;
   int cols = 2;
+  // S21Matrix other(4, 4);
   // S21Matrix basic(rows, cols);
   S21Matrix basic;
+  // S21Matrix other(4, 4);
 
-  // basic(0, 0) = 1.0;
-  // basic(0, 1) = 22.0;
-  // basic(1, 0) = 333.0;
-  // basic(1, 1) = 444.0;
+  basic(0, 0) = 1.0;
+  basic(0, 1) = 22.0;
+  basic(1, 0) = 333.0;
+  basic(1, 1) = 444.0;
+
+  S21Matrix other(move(basic));
 
   // S21Matrix *ptr_basic = &basic;
   // ptr_basic->S21Matrix();
   for (int i = 0; i < rows; i++) {
     for (int j = 0; j < cols; j++) {
-      cout << basic(i, j) << " ";
+      cout << other(i, j) << " ";
     }
     cout << endl;
   }
--- src/s21_matrix_oop.h
+++ src/s21_matrix_oop.h
@@ -1,5 +1,5 @@
-// #define SRC_S21_MATRIX_OOP_H_
 #ifndef SRC_S21_MATRIX_OOP_H_
+#define SRC_S21_MATRIX_OOP_H_
 
 class S21Matrix {
  private:
@@ -10,15 +10,16 @@ class S21Matrix {
   S21Matrix();
 
   S21Matrix(int rows, int cols);
-  // S21Matrix(const S21Matrix& other);
-  // S21Matrix(const S21Matrix&& other);
+  S21Matrix(const S21Matrix& other);
+  S21Matrix(const S21Matrix&& other);
 
   // ADDITIONAL FUNCTIONS:
   void aloc_matrix(int rows, int cols);
+  void null_matrix(S21Matrix other);
   // OPERTATORS:
   double& operator()(int i, int j);
 
-  // ~S21Matrix();
+  ~S21Matrix();
 };
 
 // S21Matrix ::S21Matrix(/* args */) {}
